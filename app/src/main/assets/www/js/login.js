32d7c8c4efca9e38db4c180748df0dec34f1942e
javed.khan <javedak09@gmail.com>
2018-11-03 17:17:53 +0500

    Login Validation

--- app/src/main/assets/www/js/login.js
+++ app/src/main/assets/www/js/login.js
@@ -1,11 +1,18 @@
 function ValidateFunction() {
 
-     var var_username = document.getElementById("username");
+    var var_username = document.getElementById("username");
+    var var_pass = document.getElementById("pass");
 
-     if var_username.value == "") {
-         alert("Please enter user name");
-         return false;
+    if(var_username.value == "") {
+        JSScript.showToast('Please enter user name');
+        var_username.focus();
+        return false;
+    }
+    else if(var_pass.value == "") {
+        JSScript.showToast('Please enter password');
+        var_pass.focus();
+        return false;
      }
 
-     return true;
- }
+    return true;
+}
--- app/src/main/java/com/example/javedak09/myangular/MainActivity.java
+++ app/src/main/java/com/example/javedak09/myangular/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.javedak09.myangular;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        WebView client = new WebView(this);
-        client.loadUrl("file:///android_asset/www/login.html");
+        WebView client = (WebView) findViewById(R.id.WebView);
+
+        /*WebSettings webSettings = client.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");*/
+
+
+        client.getSettings().setDomStorageEnabled(true);
         client.getSettings().setJavaScriptEnabled(true);
-        setContentView(client);
+
+        client.addJavascriptInterface(new MyJavascriptInterface(this), "JSScript");
+        client.loadUrl("file:///android_asset/www/login.html");
+    }
+
+    public class MyJavascriptInterface {
+
+        Context context;
+
+        public MyJavascriptInterface(Context context) {
+            this.context = context;
+        }
+
+        @android.webkit.JavascriptInterface
+        public void getStringFromJS(String txtVal) {
+            Toast.makeText(context, "Value From JS : " + txtVal, Toast.LENGTH_LONG).show();
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(context, toast, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
